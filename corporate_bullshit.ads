8fae3f1f4487003192cbb11d889e4cc9cf592f9f
gdemont <gdemont@2917bad2-2da3-4c5d-8fd0-ad49b617b64e>
2011-06-18 10:49:49 +0000

    Another contribution by Ludovic

--- corporate_bullshit.ads
+++ corporate_bullshit.ads
@@ -3,8 +3,10 @@ generic
   dialog_mark : String;
 package Corporate_Bullshit is
 
+  function Sentence return String;
+
   function Workshop return String;
 
   function Financial_Report return String;
-  
+
 end Corporate_Bullshit;
