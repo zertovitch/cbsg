62cf9a993e2db99514e06445edeae18a68f3ee63
gdemont <gdemont@2917bad2-2da3-4c5d-8fd0-ad49b617b64e>
2012-03-16 19:37:05 +0000

    Live version pimped a bit

--- live.adb
+++ live.adb
@@ -11,17 +11,23 @@ procedure Live is
    package HTML_Corporate_Bullshit is
       new Corporate_Bullshit (Paragraph => ASCII.LF & ASCII.LF & "<li>", Dialog_Mark => "");
 begin
-  Put_CGI_Header;   -- We will reply with a generated HTML document.
-  Put_HTML_Head("Minimal Form Demonstration"); -- Start generating HTML.
+  Put_CGI_Header;
+  Put_HTML_Head("Corporate Bullshit Generator");
   -- We ignore CGI.Input_Received (in a later version, we will include some input :-) )
+  Put_Line("<font face=""Calibri, Arial"">");
   Put_Line("<h1>The Corporate Bullshit Generator</h1>");
   Put_Line(
-    "<form method=""POST"">Click here for more bullshit!" &
+    "<form method=""POST"">Click here for more bullshit !" &
     " &rarr; &rarr; &rarr; <input type=""submit""></form>"
   );
-  Put_Line("The CBSG project site is here: <a href=http://sf.net/projects/cbsg/>here</a>");
+  Put_Line("The CBSG project site is <a href=http://sf.net/projects/cbsg/>here</a>.");
   Put_Line("<hr><ul>");
   Put_Line(HTML_Corporate_Bullshit.Short_Workshop);
   Put_Line("</ul>");
-  Put_HTML_Tail;  -- End the HTML document, sending </body></html>
+  Put_Line("</font><hr>");
+  Put_Line("<center>");
+  Put_Line("<img align=absmiddle src=http://sflogo.sf.net/sflogo.php?group_id=557838&type=16>");
+  Put_Line("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img align=absmiddle src=durada.gif>");
+  Put_Line("</center>");
+  Put_HTML_Tail;
 end Live;
